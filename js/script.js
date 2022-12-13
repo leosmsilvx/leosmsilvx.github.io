556b74e43de976fef492d54579018b1197f574b7
leosmsilvx <leosmsilvx@gmail.com>
2022-12-13 16:38:27 -0300

    more texts animations

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
-//Call on start
-mostrarTextos();
-let noViewTxt = true;
-
-
-//"Typing" text
+//Vars
 const cursorInputB = document.getElementsByClassName("input-cursor-b");
 const cursorInputS = document.getElementsByClassName("input-cursor-s");
 const cursorInputSW = document.getElementsByClassName("input-cursor-sw");
+const cursorInputBR = document.getElementsByClassName("input-cursor-br");
+const cursorInputSR = document.getElementsByClassName("input-cursor-sr");
+let noViewTxt = true;
 
+//Call on start
+mostrarTextos();
+mostrarOutrosTextos();
+
+//"Typing" texts
 async function escreverTexto(sentence, spanId, spanCursor, delay) {
     const letras = sentence.split("");
     let i = 0;
@@ -33,12 +36,26 @@ async function mostrarTextos(){
     await esperarTempo(1);
     await escreverTexto("Leonardo Silva", "#myName", cursorInputB, 120);
     await esperarTempo(200);
-    escreverTexto("Dev Jr.", "#devJr", cursorInputS, 120);
+    await escreverTexto("Dev Jr.", "#devJr", cursorInputS, 120);
+    continueBlinking(cursorInputS);
+}
+
+async function mostrarOutrosTextos(){   
+    await esperarTempo(1); 
+    await escreverTexto("Alguns Repositórios em que trabalhei:", "#repositoriesTxt", cursorInputBR, 70);
+    await escreverTexto("Todos os projetos são públicos e de uso educacional.", "#subtitleTxt", cursorInputSR, 40);
+    continueBlinking(cursorInputSR);
 }
 
 function mediasAnimation(){
     document.getElementById("animationRight").style.animation="slideR 1.8s ease-out both";
     document.getElementById("animationLeft").style.animation="slideL 1.8s ease-out both";
+}
+
+function continueBlinking(spanCursor){
+    for (const element of spanCursor) {
+        element.style.display = 'inline-block';
+    }
 }
 
 //Call animation just when is in user view
