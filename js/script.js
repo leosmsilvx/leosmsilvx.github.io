39cea73e2f38f74c25cb829c892ccc6a0de74189
leosmsilvx <leosmsilvx@gmail.com>
2022-12-12 23:11:53 -0300

    add more animation

--- js/script.js
+++ js/script.js
@@ -1,12 +1,25 @@
+//Call on start
+mostrarTextos();
+let noView = true;
+
+//"Typing" text
 const cursorInputB = document.getElementsByClassName("input-cursor-b");
 const cursorInputS = document.getElementsByClassName("input-cursor-s");
+const cursorInputSW = document.getElementsByClassName("input-cursor-sw");
+
 async function escreverTexto(sentence, spanId, spanCursor) {
     const letras = sentence.split("");
     let i = 0;
+    for (const element of spanCursor) {
+        element.style.display = 'inline-block';
+    }
     while(i < letras.length) {
       await esperarTempo(100);
       $(spanId).append(letras[i]);      
       i++
+    }
+    for (const element of spanCursor) {
+        element.style.display = 'none';
     }
     return false;
 }
@@ -17,18 +30,19 @@ function esperarTempo(ms) {
 
 async function mostrarTextos(){
     await esperarTempo(1);
-    for (const element of cursorInputS) {
-        element.style.display = 'none';
-    }
-    await escreverTexto("Leonardo Silva", "#myName");
-    for (const element of cursorInputB) {
-        element.style.display = 'none';
-    }
-    for (const element of cursorInputS) {
-        element.style.display = 'inline-block';
-    }
+    await escreverTexto("Leonardo Silva", "#myName", cursorInputB);
     await esperarTempo(200);
-    escreverTexto("Dev Jr.", "#devJr");
+    escreverTexto("Dev Jr.", "#devJr", cursorInputS);
 }
 
-mostrarTextos();
+//Call animation just when is in user view
+window.addEventListener('scroll', async function() {
+    var element = document.querySelector('#sclmedia');
+    var position = element.getBoundingClientRect();
+    
+    if((position.top >= 0 && position.bottom <= window.innerHeight) && noView == true) {
+        noView = false;
+        await escreverTexto("Minhas redes sociais e meios de contato:", "#sclMediaText", cursorInputSW);
+    }
+});
+
