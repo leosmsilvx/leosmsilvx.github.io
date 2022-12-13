f2e588166b371e4f574eae82388f9466bd20a23c
leosmsilvx <leosmsilvx@gmail.com>
2022-12-13 16:16:46 -0300

    fix animation

--- js/script.js
+++ js/script.js
@@ -8,14 +8,14 @@ const cursorInputB = document.getElementsByClassName("input-cursor-b");
 const cursorInputS = document.getElementsByClassName("input-cursor-s");
 const cursorInputSW = document.getElementsByClassName("input-cursor-sw");
 
-async function escreverTexto(sentence, spanId, spanCursor) {
+async function escreverTexto(sentence, spanId, spanCursor, delay) {
     const letras = sentence.split("");
     let i = 0;
     for (const element of spanCursor) {
         element.style.display = 'inline-block';
     }
     while(i < letras.length) {
-      await esperarTempo(100);
+      await esperarTempo(delay);
       $(spanId).append(letras[i]);      
       i++
     }
@@ -31,13 +31,14 @@ function esperarTempo(ms) {
 
 async function mostrarTextos(){
     await esperarTempo(1);
-    await escreverTexto("Leonardo Silva", "#myName", cursorInputB);
+    await escreverTexto("Leonardo Silva", "#myName", cursorInputB, 120);
     await esperarTempo(200);
-    escreverTexto("Dev Jr.", "#devJr", cursorInputS);
+    escreverTexto("Dev Jr.", "#devJr", cursorInputS, 120);
 }
 
 function mediasAnimation(){
-    document.getElementById("animationRedes").style.animation="slide 2s ease-out both";
+    document.getElementById("animationRight").style.animation="slideR 1.8s ease-out both";
+    document.getElementById("animationLeft").style.animation="slideL 1.8s ease-out both";
 }
 
 //Call animation just when is in user view
@@ -47,7 +48,7 @@ window.addEventListener('scroll', async function() {
     
     if((positionTextScl.top >= 0 && positionTextScl.bottom <= window.innerHeight) && noViewTxt == true) {
         noViewTxt = false;
-        escreverTexto("Minhas redes sociais e meios de contato:", "#sclMediaText", cursorInputSW);
+        escreverTexto("Minhas redes sociais e meios de contato:", "#sclMediaText", cursorInputSW, 70);
         mediasAnimation();
     }
 });
