5abd8775da78d79981033d1175238fb9204fa2a5
Leonardo Moura <leonardo.moura@pdcase.com.br>
2025-07-24 15:54:28 -0300

    Publicação angular

--- server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-J5FCKY6O.mjs";import"./chunk-Y353QOL3.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
--- src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { LandingComponent } from './landing/landing.component';
-
-export const routes: Routes = [
-    {path: '', component: LandingComponent},
-    {path: '**', redirectTo: ''},
-];
